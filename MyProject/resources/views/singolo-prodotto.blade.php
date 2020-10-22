40de30011b187033ce5bcaadd50e7d1b22375315
Alessio Bonetti <alesine87@gmail.com>
2020-10-22 18:52:48 +0200

    problema descrizione con OL risolto

--- MyProject/resources/views/singolo-prodotto.blade.php
+++ MyProject/resources/views/singolo-prodotto.blade.php
@@ -6,7 +6,9 @@
             <h1>{{$data["titolo"]}}</h1>
             <img src="{{$data["src-h"]}}" alt="" srcset="">
             <img src="{{$data["src-p"]}}" alt="" srcset="">
-            <p>{!!$data["descrizione"]!!}</p>
+            <div class="description">
+                <p>{!!$data["descrizione"]!!}</p>
+            </div>
         </div>
     </div>
 @endsection
